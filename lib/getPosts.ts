8083c63617353e6df6652de0a985452ec145a3d9
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-11-30 13:14:42 +0100

    Add error handling

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -4,27 +4,26 @@ import { readDir } from "./utils";
 
 function getPosts(path: string): Array<Post> {
   let routesArray: Post[] = [];
-  try {
-    const years = readDir(`${path}`);
-    years.forEach((year: string) => {
-      const months = readDir(`${path}/${year}`);
-      months.forEach((month: string) => {
-        const days = readDir(`${path}/${year}/${month}`);
-        days.forEach((day: string) => {
-          const files = readDir(`${path}/${year}/${month}/${day}`);
-          files.forEach((file: string) => {
-            const fsRoute = `${path}/${year}/${month}/${day}/${file}`;
-
+  const years = readDir(`${path}`);
+  years.forEach((year: string) => {
+    const months = readDir(`${path}/${year}`);
+    months.forEach((month: string) => {
+      const days = readDir(`${path}/${year}/${month}`);
+      days.forEach((day: string) => {
+        const files = readDir(`${path}/${year}/${month}/${day}`);
+        files.forEach((file: string) => {
+          const fsRoute = `${path}/${year}/${month}/${day}/${file}`;
+          try {
             const post = parsePost(fsRoute);
-
             routesArray.push(post);
-          });
+          } catch (err) {
+            console.log(err);
+          }
         });
       });
     });
-  } finally {
-    return routesArray;
-  }
+  });
+  return routesArray;
 }
 
 function createRoutesArray() {
