0370cfa8981741bcb7570568ecfcf8f0f2b5fd21
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-06-25 22:40:20 +0200

    Check whole extension

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -10,7 +10,7 @@ function getPosts(path: string): Array<Post> {
       const days = readDir(`${path}/${year}/${month}`).reverse();
       days.forEach((day: string) => {
         let files = readDir(`${path}/${year}/${month}/${day}`).reverse();
-        files = files.filter((file: string) => file.substr(file.length - 2) === "md");
+        files = files.filter((file: string) => file.substr(file.length - 3) === ".md");
         files.forEach((file: string) => {
           const fsRoute = `${path}/${year}/${month}/${day}/${file}`;
           try {
